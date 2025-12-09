f1b8936ba7faec4373f4a661fa27ea6fc246fec7
iganya <matthewiganga@gmail.com>
2025-12-09 23:32:37 +0100

    chore: change run config for deployment

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+# import uvicorn
+
+# if __name__ == "__main__":
+#     uvicorn.run(
+#         "app.main:app",
+#         host="localhost",
+#         port=8000,
+#         reload=True,
+#         # ssl_keyfile="certs/localhost-key.pem",
+#         # ssl_certfile="certs/localhost.pem"
+#     )
+
+import os
 import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
-        host="localhost",
-        port=8000,
-        reload=True,
-        # ssl_keyfile="certs/localhost-key.pem",
-        # ssl_certfile="certs/localhost.pem"
+        host="0.0.0.0",  # IMPORTANT
+        port=int(os.environ.get("PORT", 8000))  # Use Railway's port
     )
-
