5c1fae283466c73886a6d3f521dd1f3ec8157e7b
iganya <matthewiganga@gmail.com>
2025-12-10 09:28:31 +0100

    feat(main): add custom error for validated feilds

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request, status
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 
 from .core.db import engine, Base
@@ -10,16 +11,34 @@ from .core.config import config
 # Create tables
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(title="Wallet Service with Google Auth Signin")
 
 app.add_middleware(SessionMiddleware, secret_key=config.SECRET_KEY)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
+async def custom_validation_handler(request: Request, exc: RequestValidationError):
+    errors = []
+
+    for err in exc.errors():
+        field_path = ".".join(str(loc) for loc in err["loc"] if loc != "body")
+
+        errors.append({
+            "field": field_path,
+            "message": err.get("msg", "Invalid input") #pydantic default message
+        })
+
     return JSONResponse(
-        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,  # Or another status code
-        content={"error": "Validation failed"},
-        )
+        status_code=422,
+        content={"errors": errors}
+    )
 
 
 app.include_router(routes.router)
